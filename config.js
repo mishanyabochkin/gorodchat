d4f878d437519a59134f484677f6162ab5515011
mishanyabochkin <167610801+mishanyabochkin@users.noreply.github.com>
2026-09-22 00:19:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
 // const SUPABASE_URL = "https://xxxxxxxx.supabase.co";
 // const SUPABASE_ANON_KEY = "eyJ...";
 
-const SUPABASE_URL = "";
-const SUPABASE_ANON_KEY = "";
+const SUPABASE_URL = "https://trnbhxugjtaesapcedkk.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_fKhDHAOdjB-iehjpIKSbWQ_S2tfrMkD";
